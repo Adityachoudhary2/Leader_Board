c07989359bdfcff6ffbc16aa3693694efc3b3a96
Aditya <choudharyaditya9381@gmail.com>
2025-01-31 12:47:50 +0530

    Start Leader Board

---
+++ components/flatlist.tsx
@@ -0,0 +1,73 @@
+// import { ScrollView, StyleSheet, Text, View } from "react-native";
+// import React from "react";
+
+// const index = () => {
+//   return (
+//     <ScrollView
+//       style={styles.container}
+//       showsVerticalScrollIndicator={false}
+//       horizontal
+//     >
+//       {[1, 2, 3, 4, 56, 78, , 45, 4, 66, 35, 556, 78, 78].map((item) => {
+//         return (
+//           <View
+//             style={{
+//               width: 100,
+//               height: 100,
+//               backgroundColor: "red",
+//               marginBottom: 20,
+//               marginRight: 20,
+//               borderRadius:"50%",
+
+//             }}
+//           ></View>
+//         );
+//       })}
+//     </ScrollView>
+//   );
+// };
+
+// export default index;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginRight: 20,
+//     marginBottom: 20,
+//     // justifyContent: "center",
+//     // alignItems: "center",
+//   },
+// });
+
+
+// FlatList-----------------------------------------------
+
+// import { FlatList, StyleSheet, Text, View } from "react-native";
+// import React from "react";
+
+// const index = () => {
+//   return (
+//     <View>
+//       <FlatList
+//         data={[1, 2, 3, 4, 5, 56, 6, 99, 6, 5, 43, 2, 23, 467]}
+//         renderItem={() => {
+//           return (<View style={{ width: 100, height: 100, backgroundColor: 'red', marginBottom: 20, marginRight: 20, }}>
+
+//           </View>);
+//         }} />
+
+//     </View>
+//   );
+// };
+
+// export default index;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginRight: 20,
+//     marginBottom: 20,
+//     // justifyContent: "center",
+//     // alignItems: "center",
+//   },
+// });
